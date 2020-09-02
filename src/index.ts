544beb3ad19871950cbbe5be28be991f7e9bc4c3
sirlocust <jua56yu@hotmail.com>
2020-09-02 09:36:17 -0500

    creando primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-
-
-console.log('jualn');
+import { Observable} from 'rxjs'
 
 
 
 
+const obs$:Observable<string> = new Observable( subs => {
+
+    subs.next("Hola")
+    subs.next("Mundo")
+
+    subs.complete();
+
+    subs.next("Hola")
+
+
+
+});
+
+
+
+obs$.subscribe( console.log )
 
 
 
