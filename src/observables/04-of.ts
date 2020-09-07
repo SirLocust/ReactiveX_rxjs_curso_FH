2c09d7db22fb2bc845fb440dd52e425d461a51d5
sirlocust <jua56yu@hotmail.com>
2020-09-07 11:22:39 -0500

    of

---
+++ src/observables/04-of.ts
@@ -0,0 +1,18 @@
+
+import {of} from 'rxjs'
+
+
+// const obs$= of(1,2,3,4,5,6);
+const obs$= of(1,2,3,4,5,6);
+
+
+console.log('inicio del obs')
+obs$.subscribe(
+    next => console.log(next,'next'),
+    null,
+     () => console.log('terminamos')
+
+
+)
+
+console.log('fin del obs')
